4df10b91ba2e8637f7119b370a95cfeaf9e9b473
Ricky Bustillos <henrique.ssbl@hotmail.com>
2021-09-18 11:20:58 -0300

    Developed: getAll Country feature

---
+++ src/main/java/one/digitalinnovation/citiesapi/entity/Country.java
@@ -0,0 +1,33 @@
+package one.digitalinnovation.citiesapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "pais")
+public class Country {
+
+    @Id
+    private Long id;
+
+    @Column(name = "nome")
+    private String name;
+
+    @Column(name = "nome_pt")
+    private String portugueseName;
+
+    @Column(name = "sigla")
+    private String code;
+
+}
